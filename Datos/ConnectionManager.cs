bd347ea9f5cf3d126c467a9224de8f169a719764
videseva <esvides@unicesar.edu.co>
2021-04-20 00:27:06 -0500

    Proyecto con base de datos

--- Datos/ConnectionManager.cs
+++ Datos/ConnectionManager.cs
@@ -1,12 +1,18 @@
 using System.Data.SqlClient;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 namespace Datos
 {
     public class ConnectionManager
     {
-        internal SqlConnection _conexion;
+        //internal SqlConnection _conexion;
+        internal MySqlConnection _conexion;
+        
         public ConnectionManager(string connectionString)
         {
-            _conexion = new SqlConnection(connectionString);
+            //_conexion = new SqlConnection(connectionString);
+            _conexion = new MySqlConnection(connectionString);
         }
         public void Open()
         {
--- Datos/PersonaRepository.cs
+++ Datos/PersonaRepository.cs
@@ -3,11 +3,17 @@ using Entidad;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace Datos
 {
     public class PersonaRepository
     {
-        private readonly SqlConnection _connection;
+        //private readonly SqlConnection _connection;
+        private readonly MySqlConnection _connection;
         private readonly List<Persona> _personas = new List<Persona>();
         public PersonaRepository(ConnectionManager connection)
         {
@@ -15,7 +21,18 @@ namespace Datos
         }
         public void Guardar(Persona persona)
         {
-            using (var command = _connection.CreateCommand())
+            persona.CalcularPulsaciones();
+             string sql = @"Insert Into Persona (Identificacion,Nombre,Edad, Sexo, Pulsacion) 
+                                        values (@Identificacion,@Nombre,@Edad,@Sexo,@Pulsacion)";
+            MySqlCommand cmd = new MySqlCommand(sql, _connection);
+            cmd.Parameters.AddWithValue("@Identificacion", persona.Identificacion);
+            cmd.Parameters.AddWithValue("@Nombre", persona.Nombre);
+            cmd.Parameters.AddWithValue("@Sexo", persona.Sexo);
+            cmd.Parameters.AddWithValue("@Edad", persona.Edad);
+            cmd.Parameters.AddWithValue("@Pulsacion", persona.Pulsacion);
+            cmd.ExecuteNonQuery();
+
+           /* using (var command = _connection.CreateCommand())
             {
                 command.CommandText = @"Insert Into Persona (Identificacion,Nombre,Edad, Sexo, Pulsacion) 
                                         values (@Identificacion,@Nombre,@Edad,@Sexo,@Pulsacion)";
@@ -25,22 +42,38 @@ namespace Datos
                 command.Parameters.AddWithValue("@Edad", persona.Edad);
                 command.Parameters.AddWithValue("@Pulsacion", persona.Pulsacion);
                 var filas = command.ExecuteNonQuery();
-            }
+            }*/
         }
         public void Eliminar(Persona persona)
-        {
-            using (var command = _connection.CreateCommand())
+        {   
+             string sql = "Delete from persona where Identificacion=@Identificacion";
+            MySqlCommand cmd = new MySqlCommand(sql, _connection);
+            cmd.Parameters.AddWithValue("@Identificacion", persona.Identificacion);
+            cmd.ExecuteNonQuery();
+
+            /*using (var command = _connection.CreateCommand())
             {
                 command.CommandText = "Delete from persona where Identificacion=@Identificacion";
                 command.Parameters.AddWithValue("@Identificacion", persona.Identificacion);
                 command.ExecuteNonQuery();
-            }
+            }*/
         }
         public List<Persona> ConsultarTodos()
         {
+            List<Persona> personas = new List<Persona>();
+            string sql = "Select * from persona";
+            MySqlCommand cmd = new MySqlCommand(sql, _connection);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Persona persona = DataReaderMapToPerson(rdr);
+                personas.Add(persona);
+            }
+
+            /*
             SqlDataReader dataReader;
             List<Persona> personas = new List<Persona>();
-            using (var command = _connection.CreateCommand())
+           /* using (var command = _connection.CreateCommand())
             {
                 command.CommandText = "Select * from persona ";
                 dataReader = command.ExecuteReader();
@@ -52,12 +85,19 @@ namespace Datos
                         personas.Add(persona);
                     }
                 }
-            }
+            }*/
             return personas;
         }
         public Persona BuscarPorIdentificacion(string identificacion)
         {
-            SqlDataReader dataReader;
+            string sql = "select * from persona where Identificacion=@Identificacion";
+            MySqlCommand cmd = new MySqlCommand(sql, _connection);
+            cmd.Parameters.AddWithValue("@Identificacion", identificacion);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Read();
+            return DataReaderMapToPerson(rdr);
+            
+           /** SqlDataReader dataReader;
             using (var command = _connection.CreateCommand())
             {
                 command.CommandText = "select * from persona where Identificacion=@Identificacion";
@@ -65,9 +105,9 @@ namespace Datos
                 dataReader = command.ExecuteReader();
                 dataReader.Read();
                 return DataReaderMapToPerson(dataReader);
-            }
+            }*/
         }
-        private Persona DataReaderMapToPerson(SqlDataReader dataReader)
+        private Persona DataReaderMapToPerson(MySqlDataReader dataReader)
         {
             if(!dataReader.HasRows) return null;
             Persona persona = new Persona();
